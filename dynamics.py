7acfdc5b5b0ea062e1ebc2ce52a6c0a16d8f90fa
Zafar Mahmood <zfar.mah@gmail.com>
2019-01-05 18:13:34 +0100

    correction with correction 44

--- dynamics.py
+++ dynamics.py
@@ -67,10 +67,11 @@ class Dynamics(object):
             for _ in range(4):
                 x = residual(x)
             n_out_features = self.out_features.get_shape()[-1].value
-            print(" Dynamics frd loss : n_out_features {} ",np.shape(n_out_features))
+            print(" Dynamics frd loss : n_out_features ",np.shape(n_out_features))
             x = tf.layers.dense(add_ac(x), n_out_features, activation=None)
             x = unflatten_first_dim(x, sh)
-            print(" Dynamics frd loss : x unflatten_first_dim {} ",np.shape(x))
+            print(" Dynamics frd loss : x unflatten_first_dim {} , self.out_features {} ".
+                format(np.shape(x)) , np.shape(self.out_features))
             
         return tf.reduce_mean((x - tf.stop_gradient(self.out_features)) ** 2, -1)
 
